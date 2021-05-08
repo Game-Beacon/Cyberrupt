da72ef9ca003060554af5613e8f5299b885d4b51
bogay <pojay11523@gmail.com>
2021-05-09 01:48:01 +0800

    Generate turrent path overtime
    It will not pop up suddenly now.

--- Assets/Scripts/AI/Enemy/Turret/Turret.cs
+++ Assets/Scripts/AI/Enemy/Turret/Turret.cs
@@ -14,18 +14,21 @@ public class Turret : Enemy, ITarget, IStateMachine, ISpawnDanmaku
     public SpawnDanmakuHelper danmakuHelper { get { return _danmakuHelper; } }
 
     //====================
-
     [SerializeField]
     private Transform parent;
     [SerializeField]
     private Transform root;
+    [Header("Path")]
     [SerializeField]
     private LineRenderer lr;
     [SerializeField]
     private float pathLength;
     [SerializeField]
     private float pathSpeed;
-
+    [SerializeField]
+    private float pathDuration;
+    // Add this for padding
+    [Header("")]
     [SerializeField, Range(0, 0.5f)]
     private float lockStrenth;
 
@@ -37,12 +40,13 @@ public class Turret : Enemy, ITarget, IStateMachine, ISpawnDanmaku
     private Vector2 currentPosition;
     private Vector2 endPosition;
     private Vector2 pathDirection;
+    // Denote whether the path finishing generation
+    private bool isPathReady;
 
     protected override void EnemyAwake()
     {
         _stateMachine = GetComponent<AIStateMachine>();
         _danmakuHelper = GetComponent<SpawnDanmakuHelper>();
-
         _stateMachine.OnUpdateTransform.AddListener(UpdateTransform);
     }
 
@@ -50,10 +54,10 @@ public class Turret : Enemy, ITarget, IStateMachine, ISpawnDanmaku
     {
         player = DependencyContainer.GetDependency<Player>() as Player;
         _target = player.transform;
-        CreatePath();
+        StartCoroutine(CreatePath());
     }
 
-    private void CreatePath()
+    private IEnumerator CreatePath()
     {
         startPosition = transform.position;
         currentPosition = startPosition;
@@ -63,20 +67,26 @@ public class Turret : Enemy, ITarget, IStateMachine, ISpawnDanmaku
 
         lr.positionCount = 2;
         lr.SetPosition(0, currentPosition);
-        lr.SetPosition(1, endPosition);
+        for(float t = 0; t < this.pathDuration; t += Time.deltaTime)
+        {
+            lr.SetPosition(1, Vector3.Lerp(currentPosition, endPosition, t / this.pathDuration));
+            yield return null;
+        }
+        this.isPathReady = true;
     }
 
     private void UpdateTransform()
     {
+        // Freeze before path finish
+        if(!this.isPathReady)
+            return;
+        // Move
         currentPosition += pathDirection * Mathf.Min((endPosition - currentPosition).magnitude, pathSpeed * Time.fixedDeltaTime);
         root.position = currentPosition;
-
-        lr.SetPosition(0, currentPosition);
-        lr.SetPosition(1, endPosition);
-
-        if ((currentPosition - endPosition).sqrMagnitude < 0.5f)
+        // Death checking
+        if((currentPosition - endPosition).sqrMagnitude < 0.5f)
             Die();
-
+        // Update rotation
         Vector2 direction = target.position - parent.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion lookAt = Quaternion.Euler(0, 0, angle);
@@ -85,8 +95,8 @@ public class Turret : Enemy, ITarget, IStateMachine, ISpawnDanmaku
 
     public override void OnKilled()
     {
-        foreach (Enemy shield in shields)
-            if (shield != null)
+        foreach(Enemy shield in shields)
+            if(shield != null)
                 shield.Die();
         Destroy(parent.gameObject, 0.03f);
         Destroy(root.gameObject, 0.03f);
