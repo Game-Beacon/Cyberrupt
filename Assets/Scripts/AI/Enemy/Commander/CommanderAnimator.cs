43a2d39ef4cc160006e0dc2c007bb8bef2fbef5f
bogay <pojay11523@gmail.com>
2021-06-27 04:02:33 +0800

    Make commander's triangles rotate

--- Assets/Scripts/AI/Enemy/Commander/CommanderAnimator.cs
+++ Assets/Scripts/AI/Enemy/Commander/CommanderAnimator.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using UniRx;
 using UniRx.Triggers;
@@ -9,11 +12,14 @@ public class CommanderAnimator : GameBehaviour
     [SerializeField]
     private Transform barGroup;
     [SerializeField]
+    private Transform triangleGroup;
+    [SerializeField]
     private float initRotateSpeed;
     private ReactiveProperty<float> targetRatio = new ReactiveProperty<float>(1);
     private float ratio = 1;
     private float rotationSpeed => this.initRotateSpeed * this.ratio;
     private IDisposable resumeRatio;
+    private Transform[] triangles;
 
     private void AddBarTweener(Transform barRoot)
     {
@@ -69,11 +75,41 @@ public class CommanderAnimator : GameBehaviour
         {
             this.AddBarTweener(barRoot);
         }
+        // Cache triangles
+        Debug.Assert(this.triangleGroup, "Triangle group isn't set.");
+        this.triangleGroup.OnDestroyAsObservable()
+            .Subscribe(_ => this.triangleGroup.DOKill());
+        var tempTries = new List<Transform>();
+        foreach(Transform triangleRoot in this.triangleGroup)
+        {
+            var triangle = triangleRoot.GetChild(0);
+            tempTries.Add(triangle);
+            triangleRoot.OnDestroyAsObservable()
+                .Subscribe(_ => triangleRoot.DOKill());
+        }
+        this.triangles = tempTries.ToArray();
     }
 
-    public void RotateTriangle()
+    // TODO: handle concurrently calls
+    public void RotateTriangle(int step = 1, float duration = .5f)
     {
-
+        // Register triangles' tweens
+        var upTweens = DOTween.Sequence();
+        var downTweens = DOTween.Sequence();
+        foreach(var triangle in this.triangles)
+        {
+            var delta = 0.5f;
+            var originX = triangle.localPosition.x;
+            upTweens.Join(triangle.DOLocalMoveX(originX - delta, duration / 3));
+            downTweens.Join(triangle.DOLocalMoveX(originX, duration / 3));
+        }
+        // Calculate rotation
+        var angle = 120 * step;
+        var endRotation = this.triangleGroup.localRotation.eulerAngles + Vector3.forward * angle;
+        DOTween.Sequence()
+            .Append(upTweens)
+            .Append(this.triangleGroup.DOLocalRotate(endRotation, duration / 3))
+            .Append(downTweens);
     }
 
     // Smoothly spped up the rotation speed by ratio
