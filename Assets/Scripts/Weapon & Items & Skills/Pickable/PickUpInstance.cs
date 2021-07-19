6d59d46b5d9a3bf2b379fc0b731239807eab8bac
Tommy031088 <38522489+Tommy881003@users.noreply.github.com>
2021-07-19 16:40:31 +0800

    2021.07.19
    feat: 調整掉落道具的邏輯和視覺反饋
    
    調整項目：
    1. 加入Rigidbody和Point Effector，讓道具彼此之間距離太近時互斥
    2. 加入Collider，使道具碰到螢幕邊界會反彈
    3. 加入Image作為掉落道具的倒數計時
    4. 加入ParticleSystem，物品在被玩家吃掉時會生成粒子
    5. 修改PickUpInstance的腳本，實作Image外框倒數和物體閃爍的功能
    
    feat: 修改按鈕的顏色
    
    調整項目：
    1. 修改各按鈕的highlighted, selected, disabled的顏色，使差別更加顯眼

--- Assets/Scripts/Weapon & Items & Skills/Pickable/PickUpInstance.cs
+++ Assets/Scripts/Weapon & Items & Skills/Pickable/PickUpInstance.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUpInstance : GameBehaviour
 {
     [SerializeField]
+    ParticleSystem ps;
+    [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
     private SpriteRenderer iconDisplayer;
     [SerializeField]
     private SpriteRenderer background;
+    [SerializeField]
+    private Image countDownBG;
+    [SerializeField]
+    private Image countDown;
     private SpriteRenderer[] srs => new [] { iconDisplayer, background };
 
     private IPickable pickable = null;
     private float remainTime;
 
-    private LayerMask playerLayer;
+    //private LayerMask playerLayer;
 
     private void clearTween()
     {
@@ -28,40 +37,75 @@ public class PickUpInstance : GameBehaviour
 
     private IEnumerator gameStart()
     {
-        var halfTime = this.remainTime / 2;
+        float halfTime = remainTime / 2;
+        countDown.DOFillAmount(0, remainTime).SetEase(Ease.Linear);
+
         yield return new WaitForSeconds(halfTime);
-        halfTime = this.remainTime - halfTime;
-        // Start fade out
-        foreach(var sr in this.srs)
-            sr.DOFade(0.1f, halfTime)
-                .SetEase(Ease.InBounce);
-        yield return new WaitForSeconds(halfTime);
+
+        float timer = 0;
+        float nextTimer = 0;
+        float tick = 0.4f;
+        bool light = false;
+
+        while(timer < halfTime)
+        {
+            if(timer >= nextTimer)
+            {
+                tick = Mathf.Max(tick - 0.025f, 0.05f);
+                nextTimer += tick;
+                countDown.color = new Color(countDown.color.r, countDown.color.g, countDown.color.b, (light) ? 1 : 0);
+                foreach (SpriteRenderer sr in srs)
+                    sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, (light) ? 1 : 0.3f);
+                light = !light;
+            }
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        countDown.color = new Color(countDown.color.r, countDown.color.g, countDown.color.b);
+        foreach (SpriteRenderer sr in srs)
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b);
+
+        countDown.DOFade(0, 0.15f);
+        countDownBG.DOFade(0, 0.15f);
+        foreach (SpriteRenderer sr in srs)
+            sr.DOFade(0, 0.15f);
+
+        yield return new WaitForSeconds(0.15f);
+
         KillBehaviour(true);
     }
 
     public override void GameStart()
     {
+        rb.velocity = Random.insideUnitCircle.normalized * 2f;
         StartCoroutine(this.gameStart());
     }
 
-    public void InjectData(IPickable data, LayerMask mask, float remain)
+    public void InjectData(IPickable data, float remain)
     {
         pickable = data;
         iconDisplayer.sprite = pickable.icon;
-        playerLayer = mask;
         remainTime = remain;
 
         PickUpTypeData d = PickUpHelper.instance.GetData(data.type);
         background.color = d.color;
+
+        var main = ps.main;
+        main.startColor = d.color;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if((1 << collision.gameObject.layer & playerLayer) != 0)
+        if((1 << collision.gameObject.layer & CollisionLayer.instance.playerMask) != 0)
         {
             Player player = collision.gameObject.GetComponent<Player>();
             AudioManager.instance.PlaySFXOneShot(PickUpHelper.instance.GetData(pickable.type).clip);
             pickable.OnPick(player);
+
+            ps.transform.SetParent(null);
+            ps.Play();
+
             // Ensure that tweeners are killed
             this.clearTween();
             KillBehaviour(true);
